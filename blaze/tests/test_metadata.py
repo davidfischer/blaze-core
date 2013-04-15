2e2fa136f0cb7920d30d3d208ace2d8efd6ac1ca
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-04-15 16:00:00 -0500

    Convert metadata tests.

--- blaze/tests/test_metadata.py
+++ blaze/tests/test_metadata.py
@@ -1,18 +1,39 @@
 '''Tests of blaze.metadata package.'''
 import blaze
+import unittest
 
-def test_metadata_has_prop():
-    a = blaze.ones(blaze.dshape('20, 20, float64'))
-    c = blaze.NDTable([(1.0, 1.0), (1.0, 1.0)], dshape='2, {x: int32; y: float32}')
+tests = []
 
-    assert blaze.metadata.has_prop(a, blaze.metadata.arraylike)
-    assert blaze.metadata.has_prop(c, blaze.metadata.tablelike)
-    assert not blaze.metadata.has_prop(a, blaze.metadata.tablelike)
+class TestMetadata(unittest.TestCase):
 
-def test_metadata_all_prop():
-    a = blaze.ones(blaze.dshape('20, 20, float64'))
-    b = blaze.zeros(blaze.dshape('20, 20, float64'))
-    c = blaze.NDTable([(1.0, 1.0), (1.0, 1.0)], dshape='2, {x: int32; y: float32}')
+    def test_metadata_has_prop(self):
+        a = blaze.ones(blaze.dshape('20, 20, float64'))
+        c = blaze.NDTable([(1.0, 1.0), (1.0, 1.0)], dshape='2, {x: int32; y: float32}')
 
-    assert blaze.metadata.all_prop((a, b), blaze.metadata.arraylike)
-    assert not blaze.metadata.all_prop((a, b, c), blaze.metadata.arraylike)
+        self.assertTrue( blaze.metadata.has_prop(a, blaze.metadata.arraylike) )
+        self.assertTrue( blaze.metadata.has_prop(c, blaze.metadata.tablelike) )
+        self.assertTrue( not blaze.metadata.has_prop(a, blaze.metadata.tablelike) )
+
+    def test_metadata_all_prop(self):
+        a = blaze.ones(blaze.dshape('20, 20, float64'))
+        b = blaze.zeros(blaze.dshape('20, 20, float64'))
+        c = blaze.NDTable([(1.0, 1.0), (1.0, 1.0)], dshape='2, {x: int32; y: float32}')
+
+        self.assertTrue( blaze.metadata.all_prop((a, b), blaze.metadata.arraylike) )
+        self.assertTrue(not blaze.metadata.all_prop((a, b, c), blaze.metadata.arraylike))
+
+tests.append(TestMetadata)
+
+#------------------------------------------------------------------------
+
+def run(verbosity=1, repeat=1):
+    suite = unittest.TestSuite()
+    for cls in tests:
+        for _ in range(repeat):
+            suite.addTest(unittest.makeSuite(cls))
+
+    runner = unittest.TextTestRunner(verbosity=verbosity)
+    return runner.run(suite)
+
+if __name__ == '__main__':
+    run()
